0d55667f722083cb4b86a46bafacc109ab36b455
lmthong97 <lmthong97@gmail.com>
2021-07-03 01:02:43 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React shopping cart</a>
+      </header>
+
+      <main>Product list</main>
+
+      <footer>All right is reserved</footer>
+    </div>
+  );
+}
+
+export default App;
